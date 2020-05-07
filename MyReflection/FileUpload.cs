729e132bfac11c20121bc6779583bb77ef999ff7
lx4556332 <lx4556332@126.com>
2020-05-07 23:31:11 +0800

    Minio

---
+++ MyReflection/FileUpload.cs
@@ -0,0 +1,81 @@
+﻿using Minio;
+using Minio.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyReflection
+{
+    public class FileUpload
+    {
+        public async static Task Run(MinioClient minio)
+        {
+            var bucketName = "mymusic";
+            var location = "us-east-1";
+            var objectName = "hello.txt";
+            var filePath = "E:\\MinIO\\hello.txt";
+            var contentType = "application/zip";
+
+            try
+            {
+                //上传
+
+                // Make a bucket on the server, if not already present.
+                //bool found = await minio.BucketExistsAsync(bucketName);
+                //if (!found)
+                //{
+                //    await minio.MakeBucketAsync(bucketName, location);
+                //}
+
+                //byte[] binaryData = Encoding.ASCII.GetBytes("hello word");
+                //var stream = new MemoryStream(binaryData);
+
+                //await minio.PutObjectAsync(bucketName,"hello.txt", filePath);
+                // Upload a file to bucket.
+                //await minio.PutObjectAsync(bucketName, objectName, filePath, contentType);
+
+
+                //下载
+                await minio.StatObjectAsync(bucketName, objectName);
+                await minio.GetObjectAsync(bucketName, objectName,
+                (stream) =>
+                {
+
+                    using (var requestStream = stream)
+                    {
+                        using (var fs = File.Open("D:\\999.txt", FileMode.OpenOrCreate))
+                        {
+                            int length = 4 * 1024;
+                            var buf = new byte[length];
+                            do
+                            {
+                                length = requestStream.Read(buf, 0, length);
+                                fs.Write(buf, 0, length);
+                            } while (length != 0);
+                        }
+                    }
+                  });
+
+                    //var fileStream = File.Create(objectName);
+                    //stream.CopyTo(fileStream);
+                    //fileStream.Dispose();
+                    //var writtenInfo = new FileInfo(objectName);
+                    //long file_read_size = writtenInfo.Length;
+                    //writtenInfo.CopyTo("D:\\golden-oldies.zip", true);
+                    //// Uncomment to print the file on output console
+                    //// stream.CopyTo(Console.OpenStandardOutput());
+                    //Console.WriteLine($"Successfully downloaded object with requested offset and length {writtenInfo.Length} into file");
+                    //stream.Dispose();
+
+
+                    Console.WriteLine("Successfully uploaded " + objectName);
+            }
+            catch (MinioException e)
+            {
+                Console.WriteLine("File Upload Error: {0}", e.Message);
+            }
+        }
+    }
+}
--- MyReflection/Program.cs
+++ MyReflection/Program.cs
@@ -1,9 +1,12 @@
 ﻿using DB.Interface;
 using DB.Model;
 using DB.SqlServer;
+using Minio;
+using Minio.Exceptions;
 using System;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MyReflection
 {
@@ -11,31 +14,47 @@ namespace MyReflection
     {
         static void Main(string[] args)
         {
-            IDBHelper db = SimpleFactory.CreateInstance();
-            db.Query();
+            var endpoint = "127.0.0.1:9000";
+            var accessKey = "minioadmin";
+            var secretKey = "minioadmin";
+            try
+            {
+                var minio = new MinioClient(endpoint, accessKey, secretKey);
+                FileUpload.Run(minio).Wait();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
-            Assembly assembly = Assembly.Load("DB.SqlServer");
-            Type type = assembly.GetType("DB.SqlServer.GenericMethod");
-            object oTest = Activator.CreateInstance(type);
-            var Show = type.GetMethod("Show");
 
-            var showGeneric = Show.MakeGenericMethod(new Type[] { 
-                typeof(int),typeof(string),typeof(DateTime)
-            });
-            showGeneric.Invoke(oTest, new object[] { 123, "请输入账号", DateTime.Now });
+            //IDBHelper db = SimpleFactory.CreateInstance();
+            //db.Query();
+
+            //Assembly assembly = Assembly.Load("DB.SqlServer");
+            //Type type = assembly.GetType("DB.SqlServer.GenericMethod");
+            //object oTest = Activator.CreateInstance(type);
+            //var Show = type.GetMethod("Show");
+
+            //var showGeneric = Show.MakeGenericMethod(new Type[] { 
+            //    typeof(int),typeof(string),typeof(DateTime)
+            //});
+            //showGeneric.Invoke(oTest, new object[] { 123, "请输入账号", DateTime.Now });
      
 
-            SqlServerHelper help = new SqlServerHelper();
-            var company=help.Find<Company>(1);
+            //SqlServerHelper help = new SqlServerHelper();
+            //var company=help.Find<Company>(1);
 
 
-            UTF8Encoding utf8 = new UTF8Encoding();
-            Byte[] encodedBytes = utf8.GetBytes(company.Name);
-            String decodedString = utf8.GetString(encodedBytes);
+            //UTF8Encoding utf8 = new UTF8Encoding();
+            //Byte[] encodedBytes = utf8.GetBytes(company.Name);
+            //String decodedString = utf8.GetString(encodedBytes);
 
-            Console.WriteLine(decodedString);
+            //Console.WriteLine(decodedString);
 
             Console.ReadLine();
         }
+
+
     }
 }
